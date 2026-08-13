805dd2020bead6beb794e0d9fd9d00c111125bd3
VENKATANATHA AV <162578570+venk-ee@users.noreply.github.com>
2026-08-13 15:36:09 +0530

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        for i in range(n):
+            for j in range(0,n-i-1):
+                if nums[j]>nums[j+1]:
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
+
+        return nums
